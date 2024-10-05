da099c642d341a31d2b119c5950020a1a0d7e315
DuduDev <107651217+eduardomendes117@users.noreply.github.com>
2024-10-05 15:41:41 -0300

    update

--- js/app.js
+++ js/app.js
@@ -1,79 +1,42 @@
 // Google OAuth2
-function onSuccess(googleUser) {
-  console.log("Usuário logado: " + googleUser.getBasicProfile().getName());
-}
-
-function onFailure(error) {
-  console.log(error);
-}
-
-gapi.load("auth2", function () {
-  gapi.auth2.init();
-  const googleButton = document.getElementById("googleBtn");
-  googleButton.addEventListener("click", function () {
-    const auth2 = gapi.auth2.getAuthInstance();
-    auth2.signIn().then(onSuccess).catch(onFailure);
-  });
-});
-
-// function handleCredentialResponse(response) {
-//     // Decodifica o token JWT
-//     const responsePayload = parseJwt(response.credential);
-//     console.log('Decoded JWT:', responsePayload);
-
-//     // Obtém as informações do usuário
-//     const profilePicUrl = responsePayload.picture;
-//     const userName = responsePayload.name;
-//     const userEmail = responsePayload.email;
-
-//     // Atualiza os elementos da interface com as informações do usuário
-//     document.getElementById('profile-pic').src = profilePicUrl;
-//     document.getElementById('user-name').innerText = userName;
-//     document.getElementById('user-email').innerText = userEmail;
-
-//     // Mostra a div com as informações do usuário
-//     document.getElementById('user-info').style.display = 'block';
-
-//     // Redireciona o usuário após 2 segundos (2000 ms)
-//     setTimeout(() => {
-//         window.location.href = 'inicio.html'; // Altere para a página de redirecionamento desejada
-//     }, 2000);
-// }
-
-// // Função para decodificar o token JWT
-// function parseJwt(token) {
-//     const base64Url = token.split('.')[1];
-//     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-//     return JSON.parse(window.atob(base64));
-// }
-
 function handleCredentialResponse(response) {
-    // Decodifica o token JWT
-    const responsePayload = parseJwt(response.credential);
-    console.log('Decoded JWT:', responsePayload);
+  // Decodifica o token JWT
+  const data = jwt_decode(response.credential);
+  console.log(data);
 
-    // Obtém as informações do usuário
-    const profilePicUrl = responsePayload.picture;
-    const userName = responsePayload.name;
-    const userEmail = responsePayload.email;
+  // Obtém as informações do usuário
+  const picture = data.picture;
+  // const userName = data.name;
+  // const userEmail = data.email;
 
-    // Armazena os dados no localStorage
-    localStorage.setItem('profilePic', profilePicUrl);
-    localStorage.setItem('userName', userName);
-    localStorage.setItem('userEmail', userEmail);
+  // Armazena os dados no localStorage
+  localStorage.setItem("profilePic", picture);
+  // localStorage.setItem("userName", userName);
+  // localStorage.setItem("userEmail", userEmail);
 
-    // Mostra a div com as informações do usuário
-    document.getElementById('user-info').style.display = 'block';
-
-    // Redireciona o usuário após 2 segundos (2000 ms)
-    setTimeout(() => {
-        window.location.href = 'inicio.html'; // Altere para a página de redirecionamento desejada
-    },);
+  // Redireciona o usuário após 2 segundos (2000 ms)
+  setTimeout(() => {
+    window.location.href = "http://localhost:5500/pages/inicio.html"; // Altere para a página de redirecionamento desejada
+  }, 2000);
 }
 
-// Função para decodificar o token JWT
-function parseJwt(token) {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    return JSON.parse(window.atob(base64));
-}
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id:
+      "839385603142-i6vdv0tqupvv4luhb05hpah2pf6ilgo8.apps.googleusercontent.com",
+    callback: handleCredentialResponse,
+  });
+
+  google.accounts.id.renderButton(
+    document.getElementById("googleBtn"),
+    {
+      type: "standard",
+      theme: "outline",
+      text: "signin_with",
+      size: "large",
+      logo_alignment: "left",
+      width: "312px",
+    } // customization attributes
+  );
+  google.accounts.id.prompt(); // also display the One Tap dialog
+};
--- js/server.js
+++ js/server.js
@@ -1,9 +1,9 @@
 // Recupera os dados do localStorage
-const profilePicUrl = localStorage.getItem("profilePic");
+const picture = localStorage.getItem("profilePic");
 const userName = localStorage.getItem("userName");
 const userEmail = localStorage.getItem("userEmail");
 
 // Atualiza os elementos da interface com as informações do usuário
-document.getElementById("profile-pic").src = profilePicUrl;
+document.getElementById("profile-pic").src = picture;
 document.getElementById("user-name").innerText = userName;
 document.getElementById("user-email").innerText = userEmail;
